ab5d334fc9c17705f90c5470c854942e0755e542
Hadrian <hadrian.semroud@gmail.com>
2014-03-31 21:32:56 +0200

    aCommit

---
+++ Assets/Script/aggroZone.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class aggroZone : MonoBehaviour {
+
+	//Private
+	public bool aggro;
+
+	// Use this for initialization
+	void Start () {
+		aggro = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			aggro = true;
+		}
+	
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			aggro = false;
+		}
+	
+	}
+}
