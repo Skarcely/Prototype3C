8b86194aea901402483205ded5b0f7942b4a5e67
Hadrian <hadrian.semroud@gmail.com>
2014-03-31 16:04:23 +0200

    ennemyIsTargetingThePlayer

--- Assets/Script/enemyBehavior.cs
+++ Assets/Script/enemyBehavior.cs
@@ -6,18 +6,27 @@ public class enemyBehavior : MonoBehaviour {
 	//Public Var
 	public int speedEnemy;
 	public int paternMax;
+	public GameObject perso;
+	public GameObject startPoint;
 
 	//Private
 	private Vector3 pos;
 	private Vector3 initPos;
+	private Vector3 firstPos;
+	private Vector3 tempPos;
+	private Quaternion firstRotation;  
 	private bool isMoving;
 	private float patternLenght;
+	private float tempPatternLenght;
 	private Vector3 patternLenghtVector;
+	private bool hasAggro;
 
 	// Use this for initialization
 	void Start ()
 	{
 		initVar();
+		isMoving = true;
+		hasAggro = false;
 	}
 	
 	// Update is called once per frame
@@ -25,6 +34,7 @@ public class enemyBehavior : MonoBehaviour {
 		move();
 		storePos();
 		checkPattern();	
+		testAggro();
 	}
 
 	void lateUpdate()
@@ -32,6 +42,7 @@ public class enemyBehavior : MonoBehaviour {
 
 	}
 
+	//Enemy is moving every frame in front of him
 	void move()
 	{
 		if (isMoving ==  true)
@@ -41,16 +52,18 @@ public class enemyBehavior : MonoBehaviour {
 	}
 		
 
+	//Check if enemy has turn
 	void checkPattern()
 	{
 		if (patternLenght > paternMax)
 		{
-			Debug.Log("Stop");
-			isMoving = false;
+			this.transform.forward = -this.transform.forward;
+			resetPattern();
 		
 		}
 	}
 
+	//Check Pattern lenght every frame
 	void storePos()
 	{
 	 	pos = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
@@ -60,6 +73,46 @@ public class enemyBehavior : MonoBehaviour {
 	void initVar()
 	{
 		initPos = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
-		isMoving = true;
+		patternLenght = Vector3.Distance(initPos, pos);
+		firstPos = initPos;
+		firstRotation = this.transform.rotation;
+	}
+
+	//Turn back when pattern is over
+	void resetPattern()
+	{
+		initPos = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+	}
+
+	void testAggro()
+	{
+		if (((GameObject.FindObjectOfType(System.Type.GetType ("aggroZone")) as aggroZone).aggro == true) && (hasAggro == false))
+		{
+			Debug.Log("WTF");
+			//store temp var for the Caract to get back to previous Pos
+			tempPos = this.transform.position;
+			tempPatternLenght = Vector3.Distance(initPos, pos);
+			//enemy is following the caract
+			this.transform.rotation = Quaternion.LookRotation(perso.transform.position - this.transform.position);
+			hasAggro = true;
+		}
+
+		if (((GameObject.FindObjectOfType(System.Type.GetType ("aggroZone")) as aggroZone).aggro == false) && (hasAggro == true))
+		{
+			returnToPattern();
+		}
+	}
+
+	void returnToPattern()
+	{
+		Debug.Log("ok");
+		this.transform.LookAt(firstPos);
+		if (this.transform.position == firstPos)
+		{
+			Debug.Log("Ok colis bien arrivé");
+			this.transform.rotation = firstRotation;
+			hasAggro = false;
+		}
+		
 	}
 }
