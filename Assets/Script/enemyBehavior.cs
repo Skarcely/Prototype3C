23ca6142053c55ba089b4dd3f71ebe8f99f05310
Hadrian <hadrian.semroud@gmail.com>
2014-03-31 12:37:53 +0200

    enemyIsMovingAgain

---
+++ Assets/Script/enemyBehavior.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyBehavior : MonoBehaviour {
+
+	//Public Var
+	public int speedEnemy;
+	public int paternMax;
+
+	//Private
+	private Vector3 pos;
+	private Vector3 initPos;
+	private bool isMoving;
+	private float patternLenght;
+	private Vector3 patternLenghtVector;
+
+	// Use this for initialization
+	void Start ()
+	{
+		initVar();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		move();
+		storePos();
+		checkPattern();	
+	}
+
+	void lateUpdate()
+	{
+
+	}
+
+	void move()
+	{
+		if (isMoving ==  true)
+		{
+			this.transform.Translate(Vector3.forward*speedEnemy*Time.deltaTime);
+		}
+	}
+		
+
+	void checkPattern()
+	{
+		if (patternLenght > paternMax)
+		{
+			Debug.Log("Stop");
+			isMoving = false;
+		
+		}
+	}
+
+	void storePos()
+	{
+	 	pos = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+	 	patternLenght = Vector3.Distance(initPos, pos);
+	}
+
+	void initVar()
+	{
+		initPos = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+		isMoving = true;
+	}
+}
