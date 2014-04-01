30e26a6128e374b0dc7371008fac069ae91d3974
Skarcely <pinson.kevin@gmail.com>
2014-04-01 19:19:20 +0200

    Test Glitch

--- Assets/Script/Plateforms/CheckGlitch.cs
+++ Assets/Script/Plateforms/CheckGlitch.cs
@@ -3,24 +3,70 @@ using System.Collections;
 
 public class CheckGlitch : MonoBehaviour {
 
+	//Private
+	private bool isInFrame;
+	private Vector3 thisPosition;
+	private float randomWait;
 	
-	private bool isvisible;
+	
+	//Public
+	public Camera refCam;
+	public float marginRL = 0.1f;
+	public float marginHB = 0.1f;
+	public Material glitchMat;
+	public Material normalMat;
+	public float deltaGlitch = 1.0f;
 	
 	// Use this for initialization
 	void Start () {
-	
+		isInFrame = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		isvisible = renderer.isVisible;
-		Debug.Log (isvisible);
+		thisPosition = refCam.WorldToViewportPoint(this.transform.position);
+		CheckInViewPort();
+		
+		if(isInFrame)
+		{
+			Debug.Log ("Is in Frame");
+			GlitchTexture();	
+		}
 	}
 	
-	void OnBecameVisible() {
-     	Debug.Log("OnBecameVisible");  
+	void CheckInViewPort()
+	{
+	
+		if( (thisPosition.x >= marginRL && thisPosition.x <= 1 - marginRL) && (thisPosition.y <= 1 - marginHB && thisPosition.y >= marginHB))
+		{
+			isInFrame = true;
+		}
+		else
+		{
+			isInFrame = false;	
+		}
+		
 	}
-	void OnBecameInvisible() {
-     	Debug.Log("Plus visible");  
+	
+	void GlitchTexture()
+	{
+//		Debug.Log("Passe dans GlitchTexture");
+		this.transform.gameObject.renderer.material = glitchMat;
+		Debug.Log ("Before wait = " + this.transform.gameObject.renderer.material);
+		
+		randomWait = Random.Range(0,5) * deltaGlitch;
+		
+		StartCoroutine(WaitRandom(randomWait));
+		this.transform.gameObject.renderer.material = normalMat;
+		Debug.Log ("Before wait = " + this.transform.gameObject.renderer.material);
+		
 	}
+	
+	IEnumerator WaitRandom(float randomTimer)
+	{
+//		Debug.Log("Passe dans WaitRandom");
+		Debug.Log(randomTimer);
+		yield return new WaitForSeconds(randomTimer);
+	}
+	
 }
